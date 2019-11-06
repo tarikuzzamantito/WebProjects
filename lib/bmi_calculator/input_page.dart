872d01d4617adc1cb1a203d7df4d8e174d50cfc3
ASUS <tarikuzzaman1@gmail.com>
2019-11-07 00:38:23 +0600

    Adarsho Abashon 304

---
+++ lib/bmi_calculator/input_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(colour: activeCardColor),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: activeCardColor),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(colour: activeCardColor),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(colour: activeCardColor),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: activeCardColor),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: 80.0,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color colour;
+  //final double width;
+  //final double height;
+  ReusableCard({@required this.colour});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: colour,
+      ),
+    );
+  }
+}
