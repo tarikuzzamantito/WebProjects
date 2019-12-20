14938f48b1d4425d3fa04299bae808486ca6741f
Tarikuzzaman <tarikuzzaman1@gmail.com>
2019-12-20 17:15:36 +0600

    Agargaon

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,36 @@ import 'package:dev_udemy_angelayu/flash_chat/screens/chat_screen.dart';
 import 'package:dev_udemy_angelayu/flash_chat/screens/login_screen.dart';
 import 'package:dev_udemy_angelayu/flash_chat/screens/registration_screen.dart';
 import 'package:dev_udemy_angelayu/flash_chat/screens/welcome_screen.dart';
+import 'package:dev_udemy_angelayu/todoey/models/task_data.dart';
+import 'package:dev_udemy_angelayu/todoey/screens/tasks_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      /*theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
-        ),
-      ),*/
-      initialRoute: WelcomeScreen.id,
-      routes: {
-        // Navigate with named routes -> Navigator.pushNamed
-        WelcomeScreen.id: (context) => WelcomeScreen(),
-        LoginScreen.id: (context) => LoginScreen(),
-        RegistrationScreen.id: (context) => RegistrationScreen(),
-        ChatScreen.id: (context) => ChatScreen(),
-      },
+    return ChangeNotifierProvider(
+      create: (context) => TaskData(),
+      //builder: (context) => TaskData(),
+      child: MaterialApp(
+        /*theme: ThemeData.dark().copyWith(
+          textTheme: TextTheme(
+            body1: TextStyle(color: Colors.black54),
+          ),
+        ),*/
+        //initialRoute: WelcomeScreen.id,
+        initialRoute: TasksScreen.id,
+        routes: {
+          // Navigate with named routes -> Navigator.pushNamed
+          WelcomeScreen.id: (context) => WelcomeScreen(),
+          LoginScreen.id: (context) => LoginScreen(),
+          RegistrationScreen.id: (context) => RegistrationScreen(),
+          ChatScreen.id: (context) => ChatScreen(),
+          TasksScreen.id: (context) => TasksScreen(),
+        },
+      ),
     );
   }
 }
---
+++ lib/todoey/screens/tasks_screen.dart
@@ -0,0 +1,80 @@
+import 'package:dev_udemy_angelayu/todoey/screens/add_task_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:dev_udemy_angelayu/todoey/widgets/tasks_list.dart';
+import 'package:provider/provider.dart';
+import 'package:dev_udemy_angelayu/todoey/models/task_data.dart';
+
+class TasksScreen extends StatelessWidget {
+  static const String id = '/tasks';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) => AddTaskScreen(),
+          );
+        },
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 30.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '${Provider.of<TaskData>(context).taskCount} Tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+              child: TasksList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
